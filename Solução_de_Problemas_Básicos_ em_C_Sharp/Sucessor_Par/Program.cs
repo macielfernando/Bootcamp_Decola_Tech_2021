fe0dc5ea1846b1aae529eb3df2ff3f6aacc8a18c
Fernando Maciel <95414754+nandomaciell@users.noreply.github.com>
2021-12-14 12:21:12 -0300

    Add files via upload

---
+++ Solução_de_Problemas_Básicos_ em_C_Sharp/Sucessor_Par/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_SucessorPar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+
+            if (x % 2 == 0)
+            {
+                x = x + 2;
+                Console.WriteLine(x);
+
+            }
+            else
+            {
+                x = x + 1;
+                Console.WriteLine(x);
+
+            }      
+        }
+    }
+}
