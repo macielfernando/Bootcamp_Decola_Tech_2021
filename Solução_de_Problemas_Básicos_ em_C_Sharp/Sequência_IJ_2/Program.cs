dc1f569bcd93ab40b1d607ea74fa87a300ac05ba
Fernando Maciel <95414754+nandomaciell@users.noreply.github.com>
2021-12-14 12:32:10 -0300

    Add files via upload

---
+++ Solução_de_Problemas_Básicos_ em_C_Sharp/Sequência_IJ_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequencia_iJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i += 2)
+            {
+                for (int I = i, j = 7; j >= 5; j -= 1)    //escreva seu código nos espaços em branco
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                }
+            }
+        }
+    }
+}
