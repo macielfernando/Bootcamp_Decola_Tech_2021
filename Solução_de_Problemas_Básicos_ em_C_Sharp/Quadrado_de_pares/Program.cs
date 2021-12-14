8c2ba9568282f9055dc671f8eb281fcb077bf767
Fernando Maciel <95414754+nandomaciell@users.noreply.github.com>
2021-12-14 12:36:16 -0300

    Add files via upload

---
+++ Solução_de_Problemas_Básicos_ em_C_Sharp/Quadrado_de_pares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrado_de_Pares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= n; x++)
+            {
+                if (x % 2 == 0)
+                {
+                    double resultado = Math.Pow(x, 2);
+                    Console.WriteLine($"{x}^2 = {resultado}");
+
+
+                }
+
+            }
+        }
+    }
+}
