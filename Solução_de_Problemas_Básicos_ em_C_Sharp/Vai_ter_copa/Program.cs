9188194a080bd4989d88e52a05d5a6ec592225b9
Fernando Maciel <95414754+nandomaciell@users.noreply.github.com>
2021-12-14 12:42:38 -0300

    Add files via upload

---
+++ Solução_de_Problemas_Básicos_ em_C_Sharp/Vai_ter_copa/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vai_ter_copa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            while ((str = Console.ReadLine()) != null)
+            {
+                if (str == null)
+                {
+
+                }
+                else          //Escreva o seu código nos espaços em branco
+                {
+                    int x = int.Parse(str);
+                    if (x == 0)
+                    {
+                        Console.WriteLine("vai ter copa!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vai ter duas!");
+                    }
+                }
+            }
+    }
+}
